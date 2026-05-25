96282ed40c4ab91d0ea82f600ece5c6ad7f44e78
Артём <Loksarr@yandex.ru>
2026-05-25 21:17:28 +0300

    Реализовать два класса

--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -11,4 +12,19 @@ public class WordleDictionary {
 
     private List<String> words;
 
+    public WordleDictionary(List<String> words) {
+        this.words = words;
+    }
+
+    public  List<String> getWords(){
+        return new ArrayList<>(words);
+    }
+
+    public boolean contains(String word){
+        return words.contains(word);
+    }
+
+    public int size(){
+        return words.size();
+    }
 }
--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -1,9 +1,45 @@
 package ru.yandex.practicum;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 этот класс содержит в себе всю рутину по работе с файлами словарей и с кодировками
     ему нужны методы по загрузке списка слов из файла по имени файла
     на выходе должен быть класс WordleDictionary
  */
 public class WordleDictionaryLoader {
+    public WordleDictionary load(String fileName) throws IOException {
+        Path path = Paths.get(fileName);
+        List<String> words = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(Files.newInputStream(path), "UTF-8"))){
+            String line;
+            while ((line = reader.readLine()) != null){
+
+                line = line.trim().toLowerCase();
+
+                if (line.length() != 5){
+                    continue;
+                }
+
+                line = line.replace("ё", "е");
+
+                words.add(line);
+            }
+        }
+
+        if (words.isEmpty()){
+            throw new RuntimeException("Словарь не содержит слов длиной 5 букв");
+        }
+
+        return new WordleDictionary(words);
+    }
 }
